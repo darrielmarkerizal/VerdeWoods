46d3aa212760e3d68c867c5a75746a9f6d41bf4f
Darriel2603 <darrielmarkerizal@gmail.com>
2023-08-19 10:02:57 +0700

    sidebar

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,27 @@
 import { Outlet } from "react-router";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import { styled } from "styled-components";
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  height: 100vh;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+`;
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
 function AppLayout() {
   return (
-    <div>
-      <p>App Layout</p>
-      <Outlet />
-    </div>
+    <StyledAppLayout>
+      <Header />
+      <Sidebar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
   );
 }
 
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,5 +1,20 @@
+import { styled } from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
+const StyledSidebar = styled.aside`
+  background-color: var(--color-grey-0);
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+  grid-row: 1/-1;
+`;
+
 function Sidebar() {
-  return <aside>SIDEBAR</aside>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
 
 export default Sidebar;

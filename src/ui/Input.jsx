15a77c71d98ef607ea203711af639f0eb366834b
Darriel2603 <darrielmarkerizal@gmail.com>
2023-08-21 11:10:52 +0700

    setup form

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Input = styled.input`
   border: 1px solid var(--color-grey-300);
-  color: var(--color-grey-0);
+  color: var(--color-grey-700);
   box-shadow: var(--shadow-sm);
   border-radius: var(--border-radius-sm);
   padding: 0.8rem 1.2rem;

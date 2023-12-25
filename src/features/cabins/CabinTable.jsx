0bd94be1116fe9b9527dc85169b2f0cee2c2b42f
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-12-25 07:04:41 +0700

    feat(cabin) : applying the render props pattern

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -33,9 +33,10 @@ function CabinTable() {
         <div>Discount</div>
         <div></div>
       </Table.Header>
-      {cabins.map((cabin) => (
-        <CabinRow key={cabin.id} cabin={cabin} />
-      ))}
+      <Table.Body
+        data={cabins}
+        render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
+      />
     </Table>
   );
 }
--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -87,7 +87,10 @@ function Row({ children }) {
     </StyledRow>
   );
 }
-function Body({ children }) {}
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No cabins found</Empty>;
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
 
 Table.Header = Header;
 Table.Row = Row;
@@ -101,6 +104,11 @@ Table.propTypes = {
 Header.propTypes = {
   columns: PropTypes.number.isRequired,
   children: PropTypes.node.isRequired,
+};
+
+Body.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  render: PropTypes.func.isRequired,
 };
 
 Row.propTypes = {

a4983d36fc1631de49983664393f2cb908278ee1
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-12-26 08:18:23 +0700

    feat(login) : working protected route

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 import { useUser } from "../features/authentication/useUser";
 import Spinner from "./Spinner";
-import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import PropTypes from "prop-types";
 
 const FullPage = styled.div`
   height: 100vh;
@@ -16,8 +16,10 @@ const FullPage = styled.div`
 function ProtectedRoute({ children }) {
   const navigate = useNavigate();
 
+  // 1. Load the authenticated user
   const { isLoading, isAuthenticated } = useUser();
 
+  // 2. If there is NO authenticated user, redirect to the /login
   useEffect(
     function () {
       if (!isAuthenticated && !isLoading) navigate("/login");
@@ -25,6 +27,7 @@ function ProtectedRoute({ children }) {
     [isAuthenticated, isLoading, navigate]
   );
 
+  // 3. While loading, show a spinner
   if (isLoading)
     return (
       <FullPage>
@@ -32,11 +35,12 @@ function ProtectedRoute({ children }) {
       </FullPage>
     );
 
+  // 4. If there IS a user, render the app
   if (isAuthenticated) return children;
 }
 
 ProtectedRoute.propTypes = {
-  children: PropTypes.element.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default ProtectedRoute;

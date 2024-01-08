cece72ca7b457b1cbaeb7cd6ffa0977b73322790
Darriel Markerizal <darrielmarkerizal@gmail.com>
2024-01-08 21:38:58 +0700

    feat(user) : sign up form

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
@@ -6,33 +7,81 @@ import Input from "../../ui/Input";
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  return (
-    <Form>
-      <FormRow label="Full name" error={""}>
-        <Input type="text" id="fullName" />
-      </FormRow>
+    const { register, formState, getValues, handleSubmit } = useForm();
+    const { errors } = formState;
 
-      <FormRow label="Email address" error={""}>
-        <Input type="email" id="email" />
-      </FormRow>
+    function onSubmit(data) {
+        console.log(data);
+    }
 
-      <FormRow label="Password (min 8 characters)" error={""}>
-        <Input type="password" id="password" />
-      </FormRow>
+    return (
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <FormRow label="Full name" error={errors?.fullName?.message}>
+                <Input
+                    type="text"
+                    id="fullName"
+                    {...register("fullName", {
+                        required: "This field is required",
+                    })}
+                />
+            </FormRow>
 
-      <FormRow label="Repeat password" error={""}>
-        <Input type="password" id="passwordConfirm" />
-      </FormRow>
+            <FormRow label="Email address" error={errors?.email?.message}>
+                <Input
+                    type="email"
+                    id="email"
+                    {...register("email", {
+                        required: "This field is required",
+                        pattern: {
+                            value: /\S+@\S+\.\S+/,
+                            message: "Please enter a valid email address",
+                        },
+                    })}
+                />
+            </FormRow>
 
-      <FormRow>
-        {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
-          Cancel
-        </Button>
-        <Button>Create new user</Button>
-      </FormRow>
-    </Form>
-  );
+            <FormRow
+                label="Password (min 8 characters)"
+                error={errors?.password?.message}
+            >
+                <Input
+                    type="password"
+                    id="password"
+                    {...register("password", {
+                        required: "This field is required",
+                        minLength: {
+                            value: 8,
+                            message: "Password must be at least 8 characters",
+                        },
+                    })}
+                />
+            </FormRow>
+
+            <FormRow
+                label="Repeat password"
+                error={errors?.passwordConfirm?.message}
+            >
+                <Input
+                    type="password"
+                    id="passwordConfirm"
+                    {...register("passwordConfirm", {
+                        required: "This field is required",
+                        validate: (value) =>
+                            value === getValues().password ||
+                            "The passwords do not match",
+                    })}
+                />
+            </FormRow>
+
+            <FormRow>
+                {/* type is an HTML attribute! */}
+                <Button variation="secondary" type="reset">
+                    Cancel
+                </Button>
+                <Button>Create new user</Button>
+            </FormRow>
+        </Form>
+    );
 }
 
 export default SignupForm;

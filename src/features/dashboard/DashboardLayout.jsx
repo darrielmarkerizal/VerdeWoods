f22f847a73662842c3c87fb13f90315e33fb3fec
Darriel Markerizal <darrielmarkerizal@gmail.com>
2024-01-08 23:35:22 +0700

    setup dashboard layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,21 @@
 import styled from "styled-components";
 
 const StyledDashboardLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto 34rem auto;
-  gap: 2.4rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: auto 34rem auto;
+    gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+    return (
+        <StyledDashboardLayout>
+            <div>Statistics</div>
+            <div>Today&apos;s activity</div>
+            <div>Chart&apos;s stay durations</div>
+            <div>Chart sales</div>
+        </StyledDashboardLayout>
+    );
+}
+
+export default DashboardLayout;
